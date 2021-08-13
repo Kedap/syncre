2e0b1c94c6aa8ca6839ca7839351ebb7a4081463
kedap <kedap.dev@protonmail.com>
2021-08-13 17:15:59 -0500

    adding testdir for run tests

--- syncre_lib/src/tests.rs
+++ syncre_lib/src/tests.rs
@@ -1,10 +1,15 @@
-use {crate::archive, std::path::Path};
+use {
+    crate::archive,
+    std::path::{Path, PathBuf},
+    testdir::testdir,
+};
 
 #[test]
 fn coping_test() {
-    let testing_file = Path::new("testing.txt");
-    let to = Path::new("testdir/folder/hello.txt");
-    match archive::copy_sync(testing_file, to) {
+    let simple_file = Path::new("testfiles/hello-world.txt");
+    let dir: PathBuf = testdir!();
+    let to = dir.join("hello-world.txt");
+    match archive::copy_sync(simple_file, to.as_path()) {
         Err(e) => panic!("{}", e),
         _ => {}
     }
@@ -12,9 +17,21 @@ fn coping_test() {
 
 #[test]
 fn coping_symbolic_link_test() {
-    let file_with_symbolic_link = Path::new("testing0.txt");
-    let to = Path::new("testdir/folder_link/hello.txt");
-    match archive::copy_sync(file_with_symbolic_link, to) {
+    let file_with_symbolic_link = Path::new("testfiles/link-hello.txt");
+    let dir: PathBuf = testdir!();
+    let to = dir.join("link-hello.txt");
+    match archive::copy_sync(file_with_symbolic_link, to.as_path()) {
+        Err(e) => panic!("{}", e),
+        _ => {}
+    }
+}
+
+#[test]
+fn coping_directory_test() {
+    let directory = Path::new("testfiles/linked");
+    let dir: PathBuf = testdir!();
+    let to = dir.join("linked");
+    match archive::copy_sync(directory, to.as_path()) {
         Err(e) => panic!("{}", e),
         _ => {}
     }
